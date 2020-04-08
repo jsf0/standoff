fb61194ec9a9252dc9ed2cfd836f36b4c9d799a3
Joseph Fierro <joseph.fierro@runbox.com>
2020-04-08 11:05:04 -0400

    Remove standoff_generator.sh from install script too

--- standoff_install.sh
+++ standoff_install.sh
@@ -2,6 +2,5 @@
 
 install -m 755 -d /usr/local/bin
 install -m 755 standoff.pl /usr/local/bin
-install -m 755 standoff_generator.sh /usr/local/bin
 install -m 755 -d /usr/local/man/man1
 install -m 644 standoff.1 /usr/local/man/man1
